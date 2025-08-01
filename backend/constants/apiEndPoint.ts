ea2db4d16023a0809300aafed6cd03f7d66989ae
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-08-01 12:04:40 +0900

    refactor : API 앤드포인트 상수화 통일

--- backend/constants/apiEndPoint.ts
+++
@@ -1,8 +0,0 @@
-export const SISE_API_ENDPOINT =
-  '/v1/kr/public/lt/real-estate-board/market-price-information' as const;
-
-export const DANJI_API_ENDPOINT =
-  '/v1/kr/public/lt/real-estate-board/estate-list' as const;
-
-export const HOUSING_PRICE_API_ENDPOINT =
-  '/v1/kr/public/lt/kras-real-estate/housing-price' as const;
