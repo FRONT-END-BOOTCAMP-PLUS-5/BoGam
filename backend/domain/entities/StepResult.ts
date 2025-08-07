28b51177fed5ce4773b2c36ce4fd90a917d5f24f
icw0201 <icw0207@naver.com>
2025-08-07 11:10:42 +0900

    feat:#44 step result 관련 api 구현 (#53)
    * feat: 스탭 결과 API 구현
    
    - GET, POST, PUT, DELETE 메서드를 사용하여 스탭 결과 조회, 생성, 수정, 삭제 기능 추가
    - 입력 유효성 검사 및 오류 처리 로직 포함
    - StepResult 관련 DTO 및 Repository 구현
    
    * refactor: query params 값 추가
    - mainNum혹은 mainNum과 subNum을 query로 입력해서
    원하는 단계의 result값만 불러올 수 있도록 변경하였음.
    프론트엔드에서의 사용 편의를 위한 추가
    
    * refactor: 스탭 결과 관련 DTO 및 API 수정
    
    - GetStepResultQueryDto를 인터페이스로 변경하여 코드 일관성 향상
    - POST 메서드에서 스탭 결과 생성 및 수정 로직을 upsert로 통합
    - 스탭 결과 조회 시 요약 정보 계산 로직 추가
    - StepResultRepository에서 findByUserAddressId, findByUserAddressAndMainNum 등의 메서드를 findByParams로 통합하여 쿼리 파라미터 처리 개선
    - 관련 오류 메시지 및 주석 업데이트
    
    * refactor: on confilct 제약조건을 위한 unique 제약 추가
    upsert 구현을 위해 step result에 unique 제약 추가
    
    * chore: 스키마 pull 및 유니크 제약 수정
    
    * refactor: 스탭 결과 API 및 DTO 수정
    
    - GET 메서드에서 입력 유효성 검사 및 오류 메시지 개선
    - POST 메서드에서 스탭 결과 생성 및 수정 로직을 upsert로 통합
    - StepResultRepository에서 mainNum과 subNum을 기반으로 stepId를 찾는 로직 추가
    - StepResult 엔티티에 mainNum과 subNum 필드 추가
    - 관련 오류 메시지 및 주석 업데이트
    
    * fix: 스탭 결과 API 입력 유효성 검사 개선
    
    - stepId 또는 mainNum+subNum 중 하나가 필수임을 명시하는 오류 메시지 추가
    
    * refactor: 스탭 결과 API 및 DTO 개선
    
    - POST 메서드 upsert함수 적용
    - StepResultUseCase에서 getStepResults 메서드 통합 및 요약 정보 계산 로직 추가

---
+++ backend/domain/entities/StepResult.ts
@@ -0,0 +1,13 @@
+export class StepResult {
+  constructor(
+    public id?: number,
+    public userAddressId?: number,
+    public stepId?: number,
+    public mismatch?: number | null,
+    public match?: number | null,
+    public unchecked?: number | null,
+    public createdAt?: Date,
+    public mainNum?: number,
+    public subNum?: number
+  ) {}
+} 
