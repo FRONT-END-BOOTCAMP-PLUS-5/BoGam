43fb81ae382a5ca8cc90a0e3c83f4bca047495ac
printseungjoo <printseungjoo@gmail.com>
2025-08-12 14:15:54 +0900

    chore: Broker entities를 interface에서 class로 수정

--- backend/domain/entities/Broker.ts
+++ backend/domain/entities/Broker.ts
@@ -1,14 +1,16 @@
-export interface BrokerEntity {
-    idCode: number,
-    idCodeNm: string,
-    jurirno: string,
-    bsnmCmpnm: string,
-    brkrNm: string,
-    brkrAsortCode: number,
-    brkrAsortCodeNm: string,
-    crqfcNo: string,
-    crqfcAcqdt: Date,
-    ofcpsSeCode: string,
-    ofcpsSeCodeNm: string,
-    lastUpdtDt: Date
+export class BrokerEntity {
+    constructor(
+        public idCode: number,
+        public idCodeNm: string,
+        public jurirno: string,
+        public bsnmCmpnm: string,
+        public brkrNm: string,
+        public brkrAsortCode: number,
+        public brkrAsortCodeNm: string,
+        public crqfcNo: string,
+        public crqfcAcqdt: Date,
+        public ofcpsSeCode: string,
+        public ofcpsSeCodeNm: string,
+        public lastUpdtDt: Date
+    ) {}
 }
