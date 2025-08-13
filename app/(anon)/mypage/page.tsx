330f766551177e25f9785fab0659da3fec51901d
aiden0413 <aiden0413@naver.com>
2025-08-13 11:00:50 +0900

    refactor: getUserAddressIdByNickname을 getUserAddressId로 함수명 변경

--- app/(anon)/mypage/page.tsx
+++ app/(anon)/mypage/page.tsx
@@ -1,14 +1,3 @@
-import GuideResultSummary from './_components/GuideResultSummary';
-
 export default function MyPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold text-gray-900 mb-8 text-center">마이페이지</h1>
-        
-        {/* 가이드 결과 요약 컴포넌트 */}
-        <GuideResultSummary />
-      </div>
-    </div>
-  );
+  return null;
 }
