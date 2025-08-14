43c7098188f808279a2d036a029aebf7d1dc1699
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-08-14 16:28:39 +0900

    chore : react-query 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import './globals.css';
-// import { Providers } from './providers';
+import { Providers } from './providers';
 import { ConfirmModal } from './(anon)/_components/common/modal/ConfirmModal';
-import DetailSlot from './@detail/default';
 
 export default function RootLayout({
   children,
-  detail,
 }: Readonly<{
   children: React.ReactNode;
-  detail: React.ReactNode;
 }>) {
   return (
     <html lang='ko'>
@@ -22,13 +19,10 @@ export default function RootLayout({
         <link rel='icon' href='/icons/icon-192x192.svg' />
       </head>
       <body className='font-sans'>
-        {/* <Providers> */}
-        {children}
-        <ConfirmModal />
-        <DetailSlot />
-        {detail}
-        {/* </Providers> */}
-        <main className='app-shell'>{children}</main>
+        <Providers>
+          {children}
+          <ConfirmModal />
+        </Providers>
       </body>
     </html>
   );
