3cea4642abcc2111f7b7e74fe7c882ef67e1bdee
Jihye-kr <jihyeg29@gmail.com>
2025-08-17 00:09:39 +0900

    feat : JWT 토큰에 사용자 ID, 닉네임, 이메일 포함하도록 확장
    - NextAuth의 jwt 및 session 콜백을 수정하여 사용자 정보를 토큰 및 세션에 포함
    - 토큰에 id, nickname, username을 담고 세션에서도 접근 가능하도록 확장
    - 세션 기반 인증이 필요한 페이지에서 사용자 정보를 쉽게 참조할 수 있게 개선
    - 프론트엔드에서 session.user.id 등으로 사용자 식별 가능

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,5 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { prisma } from '@utils/prisma';
-import bcrypt from 'bcrypt';
+import { authOptions } from '@/api/auth/[...nextauth]/authOptions';
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.username || !credentials.password) return null;
-
-        const user = await prisma.user.findUnique({
-          where: { username: credentials.username },
-        });
-
-        if (!user) return null;
-
-        const isValid = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-        if (!isValid) return null;
-
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.username,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/signin', //우리의 로그인 페이지 -> 로그인 실패시 여기로!
-  },
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
