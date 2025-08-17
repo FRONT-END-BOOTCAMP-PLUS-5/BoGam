6bdd9acc95ab59df75f4f54fb5a2be27c4bb209e
노석준 <aiden0413@naver.com>
2025-08-17 18:07:51 +0900

    Revert "feat: Turbopack 설정 업데이트"
    This reverts commit fd9035a6c3937ec6d2edcf9615f0feb89ac50e24.

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development',
   },
-  // Turbopack 활성화 (안정화됨)
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
-  },
 };
 
 const withPWA = require('next-pwa')({
