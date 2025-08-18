6ae0be10d53482caf290439900c02205bb750ef6
Jihye-kr <jihyeg29@gmail.com>
2025-08-18 16:23:39 +0900

    refactor : 브랜드 색 적용

--- app/(anon)/_components/common/header/Header.style.ts
+++ app/(anon)/_components/common/header/Header.style.ts
@@ -1,5 +1,5 @@
 export const headerStyles = {
   wrapper:
-    'sticky top-0 z-50 w-full h-16 flex items-center justify-between border-b bg-white px-[max(var(--page-x),env(safe-area-inset-left))]',
+    'sticky top-0 z-50 w-full h-16 flex items-center justify-between border-b bg-brand-white px-[max(var(--page-x),env(safe-area-inset-left))]',
   backButton: 'text-base',
 };
