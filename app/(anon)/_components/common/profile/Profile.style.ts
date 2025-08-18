a409d79a8465c070b6576ebe4181234ddf36b9b0
Jihye-kr <jihyeg29@gmail.com>
2025-08-18 15:47:22 +0900

    refactor : profile small 크기 수정
    - profile의 small 크기 10에서 8로 축소 수정

--- app/(anon)/_components/common/profile/Profile.style.ts
+++ app/(anon)/_components/common/profile/Profile.style.ts
@@ -4,7 +4,7 @@ export const getProfileClassName = (size: 'small' | 'medium') =>
   clsx(
     'rounded-full flex items-center justify-center bg-brand-gold text-brand-white font-bold font-sans',
     {
-      'w-10 h-10 text-[1rem] ': size === 'small',
+      'w-8 h-8 text-[1rem] ': size === 'small',
       'w-16 h-16 text-[1.5rem]': size === 'medium',
     }
   );
