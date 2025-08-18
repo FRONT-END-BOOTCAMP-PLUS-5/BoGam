602cd099ecb46b8e24b276aa1e5675d5ea48b504
Jihye-kr <jihyeg29@gmail.com>
2025-08-18 16:24:07 +0900

    Feat : 사용자 닉네임 스토어 생성 및 헤더 생성 (#158)
    * refactor : 전체 레이아웃 padding 적용
    
    * feat: RootFlow 전역 상태 관리 및 초기 로딩 오류 대응
    
    - zustand로 splash/onboarding/auth 전역 상태 store 구현
    - sessionStorage 연동으로 상태 유지
    - useEffect로 초기 상태 동기화
    - hydration 오류 대응을 위해 클라이언트 렌더링 보장 처리
    
    * feat : 헤더 구조 잡는 중
    
    * refactor : profile small 크기 수정
    
    - profile의 small 크기 10에서 8로 축소 수정
    
    * refactor : profile size 변수명, 문법 등수정
    
    - profile size 변수명 수정
    - header 폴더명 안에 공백 삭제
    
    * refactor : 브랜드 색 적용

--- app/(anon)/signin/page.tsx
+++ app/(anon)/signin/page.tsx
@@ -1,8 +1,21 @@
-import { ChevronLeft } from 'lucide-react';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useRootStep } from '@libs/store/rootStepStore';
 import SigninForm from '@/(anon)/signin/_components/SigninForm';
 import styles from '@/(anon)/signin/Signin.module.css';
+import { ChevronLeft } from 'lucide-react';
 
 export default function SignIn() {
+  const router = useRouter();
+  const setStep = useRootStep((state) => state.setStep);
+
+  const handleBack = () => {
+    setStep('auth');
+    sessionStorage.setItem('step', 'auth');
+    router.push('/');
+  };
+
   return (
     <>
       <section className={styles.intro}>
@@ -13,10 +26,12 @@ export default function SignIn() {
               계정에 로그인하여 전세보감의 많은 기능을 이용해보세요!
             </p>
           </div>
-          <ChevronLeft
-            className={`${styles.icon} ${styles.arrow}`}
-            aria-hidden
-          />
+          <button onClick={handleBack} aria-label='이전'>
+            <ChevronLeft
+              className={`${styles.icon} ${styles.arrow}`}
+              aria-hidden
+            />
+          </button>
         </div>
       </section>
       <SigninForm />
