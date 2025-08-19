f8f35314988d28bade94c20e7b94fcc8f4988d56
Jihye-kr <jihyeg29@gmail.com>
2025-08-19 15:46:17 +0900

    refactor : tailwind.config 색으로 모두 수정

--- app/(anon)/_components/common/button/Button.styles.ts
+++ app/(anon)/_components/common/button/Button.styles.ts
@@ -1,11 +1,11 @@
 export const styles = {
   button: "inline-flex items-center justify-center px-4 text-sm font-medium transition mt-4 h-[48.5px] rounded-xl",
   
-  primary: "bg-brand text-white hover:bg-brand/90",
+  primary: "bg-brand text-brand-white hover:bg-brand-90",
   
-  secondary: "bg-gray-200 text-gray-900 hover:bg-gray-200",
+  secondary: "bg-brand-light-gray text-brand-black hover:bg-brand-light-gray",
   
-  ghost: "bg-transparent text-gray-600 mt-1",
+  ghost: "bg-transparent text-brand-dark-gray mt-1",
   
   fullWidth: "w-full",
   
--- app/(anon)/_components/onboarding/Splash.styles.ts
+++ app/(anon)/_components/onboarding/Splash.styles.ts
@@ -1,5 +1,5 @@
 export const styles = {
-  wrapper: "fixed inset-0 flex items-center justify-center transition-all duration-700 ease-in-out bg-[#e3eff9] pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)]",
+  wrapper: "fixed inset-0 flex items-center justify-center transition-all duration-700 ease-in-out bg-brand-light-blue pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)]",
   
   wrapperEnter: "opacity-100 blur-0",
   
@@ -9,5 +9,5 @@ export const styles = {
   
   logo: "mb-4",
   
-  subText: "text-sm text-gray-600"
+  subText: "text-sm text-brand-dark-gray"
 };
